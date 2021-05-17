36f73089cae9ce320b7a39236ef31d4eb02ccc8c
Philippe Thomas <philippe.thomas@dfki.de>
2021-05-17 15:09:25 +0200

    Better precision

--- src/main/java/dfki/de/sequences/accessions/GenBankExtractor.java
+++ src/main/java/dfki/de/sequences/accessions/GenBankExtractor.java
@@ -14,20 +14,22 @@ import java.util.regex.Pattern;
 public class GenBankExtractor {
 
     final private List<Pattern> patterns;
+    final private static String prefix="(^|[\\s\\(\\)\\[\\'\"/,])"; //>
+    final private static String suffix="(?=([\\.,\\s\\)\\(\\]\\'\":;\\-/]|$))";//|:?[ATGC]>[ATGC]
 
 
     public GenBankExtractor() {
 
         patterns = new ArrayList<>();
         //Nucleotides
-        patterns.add(Pattern.compile("([a-zA-Z]\\d{5}|[a-zA-Z]{2}\\d{6}|[a-zA-Z]{2}\\d{8})")); //1 letter + 5 numerals or 2 letters + 6 numerals or 2 letters + 8 numerals
+        patterns.add(Pattern.compile(prefix +"(?<match>[A-Z]\\d{5}|[A-Z]{2}\\d{6}|[A-Z]{2}\\d{8})" +suffix)); //1 letter + 5 numerals or 2 letters + 6 numerals or 2 letters + 8 numerals
 
         //proteins
-        patterns.add(Pattern.compile("([a-zA-Z]{3}\\d{5}|[a-zA-Z]{3}\\d{7})")); //3 letters + 5 numerals or 3 letters + 7 numerals
+        patterns.add(Pattern.compile(prefix +"(?<match>[A-Z]{3}\\d{5}|[A-Z]{3}\\d{7})" +suffix)); //3 letters + 5 numerals or 3 letters + 7 numerals
 
         //WGS
-        patterns.add(Pattern.compile("([a-zA-Z]{4}(\\d{2}|\\d{6,}))")); //        4 letters + 2 numerals for WGS assembly version + 6 or more numerals or
-        patterns.add(Pattern.compile("([a-zA-Z]{6}(\\d{2}|\\d{7,}))")); //        6 letters + 2 numerals for WGS assembly version + 7 or more numerals
+        patterns.add(Pattern.compile(prefix +"(?<match>[A-Z]{4}(\\d{2}|\\d{6,}))" +suffix)); //        4 letters + 2 numerals for WGS assembly version + 6 or more numerals or
+        patterns.add(Pattern.compile(prefix +"(?<match>[A-Z]{6}(\\d{2}|\\d{7,}))" +suffix)); //        6 letters + 2 numerals for WGS assembly version + 7 or more numerals
 
     }
 
@@ -39,7 +41,7 @@ public class GenBankExtractor {
             final Matcher matcher = pattern.matcher(text);
 
             while(matcher.find()){
-                matches.add(new Entity(matcher.start(), matcher.end(), new Accession("todo", new HashSet<Types>()))); //Accession has to be compiled
+                matches.add(new Entity(matcher.start("match"), matcher.end("match"), new Accession("todo", new HashSet<Types>()))); //Accession has to be compiled
             }
         }
 
@@ -49,12 +51,13 @@ public class GenBankExtractor {
     public static void main(String[] args) {
 
 
+        String text = "(AA12345678)";
         GenBankExtractor refSeqExtractor = new GenBankExtractor();
 
-        List<Entity> entities = refSeqExtractor.findRefSeqMentions("AA12345");
+        List<Entity> entities = refSeqExtractor.findRefSeqMentions(text);
 
         for(Entity entity : entities){
-            System.out.println(entity);
+            System.out.println(text.substring(entity.getStart(), entity.getStop()));
         }
 
     }
