5e0217c6ef03c7d02d6da8f0e1db861c05c7efbf
Philippe Thomas <philippe.thomas@dfki.de>
2021-05-17 14:44:25 +0200

    Added a wrapper for both extractors

---
+++ src/main/java/dfki/de/sequences/accessions/OverallExtractor.java
@@ -0,0 +1,34 @@
+package dfki.de.sequences.accessions;
+
+import dfki.de.sequences.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OverallExtractor {
+    private GenBankExtractor extractor1 = new GenBankExtractor();
+    private RefSeqExtractor extractor2 = new RefSeqExtractor();
+
+    public List<Entity> findRefSeqMentions(String text){
+        final List<Entity> resultSet = new ArrayList<>();
+
+        resultSet.addAll(extractor1.findRefSeqMentions(text));
+        resultSet.addAll(extractor2.findRefSeqMentions(text));
+
+        return resultSet;
+    }
+
+    public static void main(String[] args) {
+
+
+        OverallExtractor refSeqExtractor = new OverallExtractor();
+
+        List<Entity> entities = refSeqExtractor.findRefSeqMentions("Hallo NM_123 und AA12345 sind cool");
+
+        for(Entity entity : entities){
+            System.out.println(entity);
+        }
+
+    }
+
+}
